c1f537a1f0130c1b4bf0c3c9ab97a9545b464f9b
qhqhqhq <quhan1130@gmail.com>
2023-03-09 13:21:11 +0800

    lab1(util): pingpong_fix

--- user/pingpong.c
+++ user/pingpong.c
@@ -6,30 +6,36 @@ int main(int argc, char **argv)
 {
     char ball = 'q';
 
-    int p[2];
-    pipe(p);
+    int p1[2];
+    pipe(p1);
+    int p2[2];
+    pipe(p2);
 
-    write(p[0], &ball, 1);
+    write(p1[1], &ball, 1);
     if (fork() == 0)
     {
         // char *buff = (char*)malloc(1);
         
-        read(p[1], (void *)0, 1);
+        read(p1[0], (void *)0, 1);
         fprintf(1, "%d: received ping\n", getpid());
-        write(p[1], (void *)0, 1);
+        write(p2[1], (void *)0, 1);
 
         // free(buff);
-        close(p[0]);
-        close(p[1]);
+        close(p1[0]);
+        close(p1[1]);
+        close(p2[0]);
+        close(p2[1]);
         exit(0);
     }
 
     // char *buff = (char*)malloc(1);
-    read(p[0], (void *)0, 1);
+    read(p2[0], (void *)0, 1);
     fprintf(1, "%d: received pong\n", getpid());
 
     // free(buff);
-    close(p[0]);
-    close(p[1]);
+    close(p1[0]);
+    close(p1[1]);
+    close(p2[0]);
+    close(p2[1]);
     exit(0);
 }
